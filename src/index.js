d3f207ec299571dd3fc3cdddb30237c362c0b72a
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-01-01 03:12:30 -0800

    Remove unused components

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import ThemedSuspense from "./components/ThemedSuspense";
 import { Windmill } from "@windmill/react-ui";
 import * as serviceWorker from "./serviceWorker";
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const axe = require('react-axe')
-//   axe(React, ReactDOM, 1000)
-// }
-
 ReactDOM.render(
   <SidebarProvider>
     <Suspense fallback={<ThemedSuspense />}>
