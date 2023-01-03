788abb4a96166afad42d4bd1e7eba2c86d8f6875
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-01-03 13:27:56 -0800

    Update sidebar to hide when screensize is md

--- src/components/Sidebar/DesktopSidebar.js
+++ src/components/Sidebar/DesktopSidebar.js
@@ -4,7 +4,7 @@ import SidebarContent from "./SidebarContent";
 
 function DesktopSidebar(props) {
   return (
-    <aside className="z-30 flex-shrink-0 hidden w-80 overflow-y-auto bg-white dark:bg-gray-800 lg:block">
+    <aside className="z-30 flex-shrink-0 hidden w-80 overflow-y-auto bg-white dark:bg-gray-800 md:block">
       <SidebarContent />
     </aside>
   );
