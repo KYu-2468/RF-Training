41d3a7760f9c74cf1126656afec478eed450a933
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-01-03 01:23:52 -0800

    Fix adb urls to correct url

--- src/pages/Automation/ADBCommand.js
+++ src/pages/Automation/ADBCommand.js
@@ -5,42 +5,22 @@ import SectionContent from "../../components/Typography/SectionContent";
 import VideoPlayerNDA from "../../components/Video/VideoPlayerNDA";
 
 const ADBCommand = () => {
-  const M2M_SUPPORT_URL =
-    "https://m2msupport.net/m2msupport/software-and-at-commands-for-m2m-modules/";
-
-  const AT_CFUN_URL =
-    "https://m2msupport.net/m2msupport/atcfun-set-phone-functionality/";
-
-  const AT_CPIN_URL = "https://m2msupport.net/m2msupport/atcpin-enter-pin/";
-
-  const AT_CGDCONT_URL =
-    "https://m2msupport.net/m2msupport/atcgdcont-define-pdp-context/";
-
-  const AT_COPS_URL =
-    "https://m2msupport.net/m2msupport/atcops-plmn-selection/";
-
+  const ADB_URL =
+    "https://www.xda-developers.com/install-adb-windows-macos-linux/";
   return (
     <>
       <PageTitle>ADB Commands</PageTitle>
       <SectionContent>
-        ADB {"(Android Debug Bridge)"}
-        <a className="text-blue-700" href={M2M_SUPPORT_URL}>
+        A great read for ADB {"(Android Debug Bridge)"} can be found here{" "}
+        <a className="text-blue-700" href={ADB_URL}>
           here
         </a>
       </SectionContent>
       <SectionContent>
         Common ADB commands:
         <ul>
-          <li>
-            <a className="text-blue-700" href={AT_CFUN_URL}>
-              adb devices
-            </a>{" "}
-          </li>
-          <li>
-            <a className="text-blue-700" href={AT_CPIN_URL}>
-              adb reboot
-            </a>
-          </li>
+          <li>adb devices - check connected devices</li>
+          <li>adb reboot - reboot connected device</li>
         </ul>
       </SectionContent>
       <VideoPlayerNDA src="at-command-example.mp4" />
